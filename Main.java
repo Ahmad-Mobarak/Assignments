fa436e578516512da05ad0bbecc91aac130033b6
Ahmad-Mobarak <125589961+Ahmad-Mobarak@users.noreply.github.com>
2023-02-21 01:12:05 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        byte Age = 18;
+        int Number = 400000000;
+        Long serialNumber = 8674376519L;
+        double dblink = 2857493.1096856;
+        float Price = 9067.69F;
+        char letter = 'A';
+        boolean classified = false;
+        System.out.println(Age);
+        System.out.println(Number);
+        System.out.println(serialNumber);
+        System.out.println(dblink);
+        System.out.println(Price);
+        System.out.println(letter);
+        System.out.println(classified);
+    }
+}
