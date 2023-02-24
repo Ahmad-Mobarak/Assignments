9f88c2f5a94909c3652c209a850f216ca82ad0e4
Ahmad-Mobarak <125589961+Ahmad-Mobarak@users.noreply.github.com>
2023-02-25 01:29:00 +0200

    Add files via upload

---
+++ Section2.java
@@ -0,0 +1,12 @@
+public class Section2
+{public static void main(String[] args) {
+    int x, y, z;
+    x = 5;
+    System.out.println("x = " + x);
+    y = 10;
+    System.out.println("y = " + y);
+    z = 15;
+    System.out.println("z = " + z);
+    System.out.println("((x+y)*z)/(x-y) = "+((x+y)*z)/(x-y));
+}
+}
