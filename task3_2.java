bdfff0ece0661772e227d7d0e2a865e23dd5d6f5
Ahmad-Mobarak <125589961+Ahmad-Mobarak@users.noreply.github.com>
2023-03-05 16:13:04 +0200

    Add files via upload

---
+++ task3_2.java
@@ -0,0 +1,8 @@
+public class task3_2 {
+    public static void main(String[] args)
+    {
+        double num = 3.14;
+        int result = (int) Math.floor(num);
+        System.out.println("The maximum integer value obtained by rounding down "+num+" is: " + result);
+    }
+}
