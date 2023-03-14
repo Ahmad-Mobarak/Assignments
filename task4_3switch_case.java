04bebcd86f17d1f3de3a2aadd8bd2b7d72001277
Ahmad-Mobarak <125589961+Ahmad-Mobarak@users.noreply.github.com>
2023-03-14 17:30:40 +0200

    Add files via upload

---
+++ task4_3switch_case.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class task4_3switch_case {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the radius of the circle: ");
+        double radius = input.nextDouble();
+
+        switch(radius > 0 ? 1 : 0) {
+            case 1:
+                double area = Math.PI * radius * radius;
+                System.out.println("The area of the circle is: " + area);
+                break;
+            case 0:
+                System.out.println("Invalid radius!");
+                break;
+        }
+
+        input.close();
+    }
+}
