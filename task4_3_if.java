8446369fd23a3a2435291126d83509e1ea0659bf
Ahmad-Mobarak <125589961+Ahmad-Mobarak@users.noreply.github.com>
2023-03-15 22:59:06 +0200

    Update task4_3_if.java

--- task4_3_if.java
+++ task4_3_if.java
@@ -1,9 +1,9 @@
 import java.util.Scanner;
 public class task4_3_if {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.print("Enter the radius of the circle: ");
-        double radius = sc.nextDouble();
+        double radius = scanner.nextDouble();
 
         if(radius > 0) {
             double area = Math.PI * radius * radius;
