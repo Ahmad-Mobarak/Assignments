67b1d303eeb262012f25d77278c4a1687a7ba820
Ahmad-Mobarak <125589961+Ahmad-Mobarak@users.noreply.github.com>
2023-03-15 22:58:23 +0200

    Update task4_2switch_case.java

--- task4_2switch_case.java
+++ task4_2switch_case.java
@@ -1,40 +1,38 @@
 import java.util.Scanner;
 public class task4_2switch_case {
     public static void main(String[] args) {
-        Scanner  input = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
         System.out.print("Enter first number: ");
-        double num1 = input.nextDouble();
+        double num1 = scanner.nextDouble();
         System.out.print("Enter second number: ");
-        double num2 = input.nextDouble();
+        double num2 = scanner.nextDouble();
 
         System.out.print("Enter operator (+, -, *, /): ");
-        char operator = input.next().charAt(0);
+        char operator = scanner.next().charAt(0);
 
-        double result;
+        double result = 0;
         switch (operator) {
             case '+':
                 result = num1 + num2;
-                System.out.println(num1 + " + " + num2 + " = " + result);
                 break;
             case '-':
                 result = num1 - num2;
-                System.out.println(num1 + " - " + num2 + " = " + result);
                 break;
             case '*':
                 result = num1 * num2;
-                System.out.println(num1 + " * " + num2 + " = " + result);
                 break;
             case '/':
                 if (num2 == 0) {
                     System.out.println("Cannot divide by zero.");
                 } else {
                     result = num1 / num2;
-                    System.out.println(num1 + " / " + num2 + " = " + result);
                 }
                 break;
             default:
-                System.out.println("Invalid operator.");
+                System.out.println("Invalid operator!");
         }
+
+        System.out.println(num1 + " " + operator + " " + num2 + " = " + result);
     }
 }
