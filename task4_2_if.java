c6028230cf1a916172fba52aacf706a7ac9df85f
Ahmad-Mobarak <125589961+Ahmad-Mobarak@users.noreply.github.com>
2023-03-15 22:57:56 +0200

    Update task4_2_if.java

--- task4_2_if.java
+++ task4_2_if.java
@@ -1,35 +1,32 @@
 import java.util.Scanner;
 public class task4_2_if {
     public static void main(String[] args) {
-        Scanner  input = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
         System.out.print("Enter first number: ");
-        double num1 = input.nextDouble();
+        double num1 = scanner.nextDouble();
         System.out.print("Enter second number: ");
-        double num2 = input.nextDouble();
+        double num2 = scanner.nextDouble();
 
         System.out.print("Enter operator (+, -, *, /): ");
-        char operator = input.next().charAt(0);
+        char operator = scanner.next().charAt(0);
 
-        double result;
+        double result = 0;
         if (operator == '+') {
             result = num1 + num2;
-            System.out.println(num1 + " + " + num2 + " = " + result);
         } else if (operator == '-') {
             result = num1 - num2;
-            System.out.println(num1 + " - " + num2 + " = " + result);
         } else if (operator == '*') {
             result = num1 * num2;
-            System.out.println(num1 + " * " + num2 + " = " + result);
         } else if (operator == '/') {
             if (num2 == 0) {
                 System.out.println("Cannot divide by zero.");
             } else {
                 result = num1 / num2;
-                System.out.println(num1 + " / " + num2 + " = " + result);
             }
         } else {
-            System.out.println("Invalid operator.");
+            System.out.println("Invalid operator!");
         }
+        System.out.println(num1 + " " + operator + " " + num2 + " = " + result);
     }
 }
