788b4827ab1a1c745dfc51a6fa76a23973a94fc5
Ahmad-Mobarak <125589961+Ahmad-Mobarak@users.noreply.github.com>
2023-03-27 13:07:46 +0200

    Add files via upload

---
+++ task5_1_do_while_Loop.java
@@ -0,0 +1,14 @@
+public class task5_1_do_while_Loop {
+    public static void main(String[] args)
+    {
+        int sum = 0;
+        int i = 1;
+        do {
+            if (i % 2 == 1) {
+                sum += i;
+            }
+            i++;
+        } while (i <= 10);
+        System.out.println("Sum of odd numbers from 1 to 10: " + sum);
+    }
+}
