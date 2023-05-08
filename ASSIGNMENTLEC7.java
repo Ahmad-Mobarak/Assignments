5cc2e01bc328375f596479794b8a09da52add3f9
Ahmad-Mobarak <125589961+Ahmad-Mobarak@users.noreply.github.com>
2023-05-08 22:44:59 +0300

    Add files via upload

---
+++ ASSIGNMENTLEC7.java
@@ -0,0 +1,18 @@
+public class ASSIGNMENTLEC7 {
+    public static void main(String[] args) {
+        int[] arr = {40, 10, -30, 45, 39, 32};
+        int n = arr.length;
+        for (int i = 1; i < n; ++i) {
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
+        for (int i = 0; i < n; ++i) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
